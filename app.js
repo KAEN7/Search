76bde080ea8f584fc00a0de6fea2672d64f11af0
kaen <kusdsuna@naver.com>
2020-11-19 23:55:11 +0900

    Add btn.innerText

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const change = document.getElementById('btn');
 const formChange = document.querySelector('search-bar');
-// const action = document.getElementById('search-bar').action;
+const searchBtn = document.getElementById('btn');
 
 function changeSearch(e) {
     $('#search-bar').attr('action','https://search.naver.com/search.naver?query=');
+    btn.innerText = 'N';
     console.log('gogo');
 }
 
