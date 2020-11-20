b1d9f03376a3d455081ac41f401a1b7d3b6e2bf4
kaen <kusdsuna@naver.com>
2020-11-20 17:03:40 +0900

    Search service activated

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const change = document.getElementById('btn');
 const formChange = document.querySelector('search-bar');
 const searchBtn = document.getElementById('btn');
 const main = document.getElementById('main');
+const naver = document.getElementById('naver');
+const google = document.getElementById('google');
 
 function changeSearch() {
     if(btn.innerText === 'G') {
         btn.innerText = 'N';
-        main.style.backgroundColor = 'green';
+        main.style.backgroundColor = 'green';     
+        google.style.display = 'none';
+        naver.style.display = 'block';
     } else {
         btn.innerText = 'G';
         main.style.backgroundColor = 'white';
+        google.style.display = 'block';
+        naver.style.display = 'none';
     }
 }
-
 
 if(change) {
     change.addEventListener('click', changeSearch);
